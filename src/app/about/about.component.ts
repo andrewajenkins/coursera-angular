00b2e59b220b0b839787eba3aa8302f8cf60e8b4
Andy Jenkins <andy@4gclinical.com>
2022-05-14 08:59:31 -0700

    RxJs Part 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -14,7 +14,7 @@ export class AboutComponent implements OnInit {
   constructor(private leaderService: LeaderService) { }
 
   ngOnInit(): void {
-    this.leaderService.getLeaders().then(leader => {
+    this.leaderService.getLeaders().subscribe(leader => {
       this.leaders = leader;
     });
   }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -10,20 +11,14 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve=> {
-        setTimeout(() => resolve(LEADERS), 2000);
-      });
-    }
-
-  getPromotion(id: string): Promise<Leader> {
-    return new Promise(resolve=> {
-      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
-    });
+  getLeaders(): Observable<Leader[]> {
+      return of(LEADERS).pipe(delay(2000));
   }
-  getFeaturedLeader(): Promise<Leader> {
-    return new Promise(resolve=> {
-      setTimeout(() => resolve(LEADERS[0]), 2000);
-    });
+
+  getPromotion(id: string): Observable<Leader> {
+      return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+  }
+  getFeaturedLeader(): Observable<Leader> {
+      return of(LEADERS[0]).pipe(delay(2000));
   }
 }
